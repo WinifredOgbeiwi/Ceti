71060c36c31550e61eabafb71458f7233b69fe5e
winifredogbeiwi <imadeogbeiwi@gmail.com>
2024-07-15 20:00:16 +0200

    Home page

--- src/pages/home/Cta2.jsx
+++ src/pages/home/Cta2.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Cta2 = ({ t }) => {
   return (
     <section className='px-5 sm:px-[72px] mt-40 bg-white'>
-      <div className='flex justify-between items-center border-[1px] border-black p-14'>
+      <div className='flex flex-col md:flex-row  gap-4 justify-between md:items-center border-[1px] border-black p-6 sm:p-14'>
         <div>
-          <h2 className=" text-3xl sm:text-[3.25rem] font-raleway font-bold">
+          <h2 className=" text-3xl sm:text-5xl font-raleway font-bold">
             {t("home.cta2.heading")}
           </h2>
-          <p className="text-base sm:text-2xl mt-6 text-secondary">
+          <p className="text-lg sm:text-2xl mt-3 text-secondary">
             {t("home.cta2.paragraph")}
           </p>
         </div>
-        <div className=' w-96 '>
-          <p className='text-lg'>{t("home.cta2.content")}</p>
+        <div className=' w-full md:w-96 '>
+          <p className='sm:text-lg'>{t("home.cta2.content")}</p>
           <button className='bg-main text-white px-5 py-3 mt-6'>{t("home.cta2.button")}</button>
         </div>
       </div>
--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -13,13 +13,13 @@ const Hero = ({ t }) => {
                     <div className="flex space-x-3 ">
                         <div className="bg-main mt-[4px] w-[11px]"></div>
                         <div>
-                            <h1 className=" font-raleway font-bold max-[436px]:text-3xl text-4xl md:text-5xl lg:text-7xl ">{t("home.herosection.heading")}</h1>
-                            <h2 className=" font-raleway mt-2 text-3xl md:text-4xl lg:text-6xl max-[436px]:text-2xl font-medium">
+                            <h1 className=" font-raleway font-bold text-4xl md:text-5xl lg:text-7xl ">{t("home.herosection.heading")}</h1>
+                            <h2 className=" font-raleway mt-2 text-3xl md:text-4xl lg:text-6xl font-medium">
                                 {t("home.herosection.subheading1")}
                             </h2>
                         </div>
                     </div>
-                    <p className="mt-7 font-roboto max-[436px]:text-xs text-sm md:text-base lg:text-xl">{t("home.herosection.paragraph")}</p>
+                    <p className="mt-7 font-roboto  md:text-base lg:text-xl">{t("home.herosection.paragraph")}</p>
                     <button className="bg-main w-fit px-5 py-4 mt-6"> {t("home.herosection.button")}</button>
                 </div>
             </div>
